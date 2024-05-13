887dbfd89923210c92caa8041b29f039cb89ad1d
yassine <yassineabida47@gmail.com>
2024-05-14 00:12:13 +0100

    my second commit

--- src/components/1-header/Header.jsx
+++ src/components/1-header/Header.jsx
@@ -1,20 +1,70 @@
+import { useState } from "react";
 import "./header.css";
 function Header() {
-
-    return (
-        <header>
-            <nav>
-                <ul>
-                    <li><a href="#">About</a></li>
-                    <li><a href="#">Articles</a></li>
-                    <li><a href="#">Projects</a></li>
-                    <li><a href="#">Speaking</a></li>
-                    <li><a href="#">Contact</a></li>
-                </ul>
-            </nav>
-                <button>click</button>
-        </header>
-    );
+  const [showModel, setShowModel] = useState(false);
+  return (
+    <header>
+      <div className="sample" />
+      <button
+        className="menu"
+        onClick={() => {
+          setShowModel(true);
+        }}
+      >
+        silce icon
+      </button>
+      <nav>
+        <ul>
+          <li>
+            <a href="#">About</a>
+          </li>
+          <li>
+            <a href="#">Articles</a>
+          </li>
+          <li>
+            <a href="#">Projects</a>
+          </li>
+          <li>
+            <a href="#">Speaking</a>
+          </li>
+          <li>
+            <a href="#">Contact</a>
+          </li>
+        </ul>
+      </nav>
+      <button>click</button>
+      {showModel && (
+        <div className="fixed">
+          <ul className="module">
+            <li>
+              <button
+                onClick={() => {
+                  setShowModel(false);
+                }}
+              >
+                click
+              </button>
+            </li>
+            <li>
+              <a href="#">About</a>
+            </li>
+            <li>
+              <a href="#">Articles</a>
+            </li>
+            <li>
+              <a href="#">Projects</a>
+            </li>
+            <li>
+              <a href="#">Speaking</a>
+            </li>
+            <li>
+              <a href="#">Contact</a>
+            </li>
+          </ul>
+        </div>
+      )}
+    </header>
+  );
 }
 
 export default Header;
